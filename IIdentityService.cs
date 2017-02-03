963da8fe236c901bff4f6053512b5113003bfeb2
Josh <josh@eastfive.com>
2017-02-03 13:28:32 -0800

    added createorupdateclaim to identity contract

--- IIdentityService.cs
+++ IIdentityService.cs
@@ -36,6 +36,8 @@ namespace EastFive.Api.Services
         TResult ParseState<TResult>(string state,
             Func<Uri, byte, byte[], TResult> onSuccess,
             Func<string, TResult> invalidState);
-
+        Task<TResult> CreateOrUpdateClaim<TResult>(string claimType, string claimValue,
+            Func<TResult> onSuccess,
+            Func<string, TResult> onFailure);
     }
 }
